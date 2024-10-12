a3dd5337a712a7b7456db435c554da57fb7d9483
Your Nameclear <you@example.com>
2024-10-12 11:42:34 +0200

    DB pool and connection

--- app.js
+++ app.js
@@ -28,6 +28,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Database connection
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+  }
+});
+
 
 
 // catch 404 and forward to error handler
