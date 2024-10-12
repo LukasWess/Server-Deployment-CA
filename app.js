4e9e89dcc53b284f319ede05685724cc2bcf2dcb
Your Nameclear <you@example.com>
2024-10-12 11:43:33 +0200

    Authentication for admin login middleware handler

--- app.js
+++ app.js
@@ -39,6 +39,19 @@ const pool = mysql.createPool({
   }
 });
 
+// Authentication middleware
+const auth = basicAuth({
+  authorizer: async (username, password) => {
+    const [rows] = await pool.query('SELECT * FROM admin_users WHERE username = ?', [username]);
+    if (rows.length > 0) {
+      const user = rows[0];
+      return await bcrypt.compare(password, user.password);
+    }
+    return false;
+  },
+  authorizeAsync: true,
+});
+
 
 
 // catch 404 and forward to error handler
